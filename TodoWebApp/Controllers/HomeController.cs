3a6ca19c7f60b7f4a86f29c767579fbe4f93fc9c
Rajesh Jella <jella.rajesh@gmail.com>
2018-07-06 19:34:56 +0100

    ..

--- TodoWebApp/Controllers/HomeController.cs
+++ TodoWebApp/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace TodoWebApp.Controllers
 {
@@ -22,7 +18,7 @@ namespace TodoWebApp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page.  ";
 
             return View();
         }
